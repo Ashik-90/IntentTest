0d88bb16ef1e33cab36c5a66d08ddfa182ec3cdc
Ashikur Rahman <58724901+Ashik-90@users.noreply.github.com>
2022-01-25 23:05:52 +0600

    Update MainActivityTest.java
    Roll added on screen

--- app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
+++ app/src/androidTest/java/com/example/simpleparadox/listycity/MainActivityTest.java
@@ -1,29 +1,34 @@
 package com.example.simpleparadox.listycity;
+
 import android.app.Activity;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import android.widget.EditText;
 import android.widget.ListView;
+
 import com.robotium.solo.Solo;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 /**
- * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
- used
+ * Test class for MainActivity. All the UI tests are written here. Robotium test framework is used
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest{
+
     private Solo solo;
+
     @Rule
     public ActivityTestRule<MainActivity> rule =
-
             new ActivityTestRule<>(MainActivity.class, true, true);
     /**
      * Runs before all tests and creates solo instance.
@@ -31,6 +36,7 @@ public class MainActivityTest{
      */
     @Before
     public void setUp() throws Exception{
+
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
     /**
@@ -47,37 +53,53 @@ public class MainActivityTest{
      */
     @Test
     public void checkList(){
-//Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
         solo.clickOnButton("ADD CITY"); //Click ADD CITY Button
-//Get view for EditText and enter a city name
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
+
+        //Get view for EditText and enter a city name
+        solo.enterText((EditText) solo.getView(R.id.editText_name), "1707090");
         solo.clickOnButton("CONFIRM"); //Select CONFIRM Button
         solo.clearEditText((EditText) solo.getView(R.id.editText_name)); //Clear the EditText
-/* True if there is a text: Edmonton on the screen, wait at least 2 seconds and
-find minimum one match. */
-        assertTrue(solo.waitForText("Edmonton", 1, 2000));
+
+        /* True if there is any text: 1707090 on the screen, wait at least 2 seconds and
+        find minimum one match. */
+        assertTrue(solo.waitForText("1707090", 1, 2000));
+
         solo.clickOnButton("ClEAR ALL"); //Select ClEAR ALL
-//True if there is no text: Edmonton on the screen
-        assertFalse(solo.searchText("Edmonton"));
+        //True if there is no text: 1707090 on the screen
+        assertFalse(solo.searchText("1707090"));
 
     }
+
     /**
      * Check item taken from the listview
      */
     @Test
     public void checkCiyListItem(){
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
         solo.clickOnButton("ADD CITY");
-        solo.enterText((EditText) solo.getView(R.id.editText_name), "Edmonton");
+        solo.enterText((EditText) solo.getView(R.id.editText_name), "1707090");
         solo.clickOnButton("CONFIRM");
-        solo.waitForText("Edmonton", 1, 2000);
-// Get MainActivity to access its variables and methods.
+        solo.waitForText("1707090", 1, 2000);
+        // Get MainActivity to access its variables and methods.
         MainActivity activity = (MainActivity) solo.getCurrentActivity();
         final ListView cityList = activity.cityList; // Get the listview
         String city = (String) cityList.getItemAtPosition(0); // Get item from first position
-        assertEquals("Edmonton", city);
+        assertEquals("1707090", city);
     }
+
+    /**
+     * switching between activities
+     */
+    @Test
+    public void switchactivities(){
+        solo.clickOnButton("Show Activity");
+        solo.clickOnButton("Main Activity");
+    }
+
     /**
      * Close activity after each test
      * @throws Exception
